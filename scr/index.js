29d6c35f408ce72581365f4cf886716806a6baf9
wazirwazir <151955963+wazirwazir@users.noreply.github.com>
2024-06-17 11:31:10 +0100

    added an array for playlist

--- scr/index.js
+++ scr/index.js
@@ -1,10 +1,12 @@
 let nextbtn = document.getElementById('nextbtn')
 let playbtn = document.getElementById('playbtn')
 let prevbtn = document.getElementById('prevbtn')
-let audio = document.getElementsByTagName('audio')[0]
+let audio = document.getElementById('audio')
 let playsign = document.querySelector('.playsign')
 let pauseign = document.querySelector('.pausesign')
 let currenAud = document.querySelector('.currentAud')
+let songTitle = document.getElementById('songTitle')
+
 
 playbtn.addEventListener('click', () => {
    if(audio.paused) {
@@ -13,8 +15,15 @@ playbtn.addEventListener('click', () => {
    else if (!audio.paused) {
     audio.pause()
    }
-   playsign.classList.toggle('hide')
-   pauseign.classList.toggle('hide')
+   if (audio.play) {
+    playsign.classList.add('hide')
+    pauseign.classList.remove('hide')
+   } else if (!audio.play) {
+    playsign.classList.remove('hide')
+    pauseign.classList.add('hide')
+   }
+   
+  
 })
 
 nextbtn.addEventListener('click', () => {
@@ -22,22 +31,37 @@ nextbtn.addEventListener('click', () => {
 })
 
 let track = [
-    '<audio src=".\scr\music\_Wizkid_-_Ojuelegba.mp3"></audio>',
-    '<audio src=".\scr\music\01 Freaky Friday (feat. Chris Brown).mp3"></audio>',
-    '<audio src=".\scr\music\01 He Is The Same.mp3"></audio>',
-    '<audio src=".\scr\music\01-Party-feat.-Gucci-Mane-Usher.mp3"></audio>',
-    '<audio src=".\scr\music\02 - Young Dumb & Broke - (Musicfire.in).mp3"></audio>'
-    
-    
+    {
+        title: 'Freaky Friday (feat. Chris Brown)',
+        file: 'scr/music/01 Freaky Friday (feat. Chris Brown).mp3'
+    },
+    {
+        title: 'Wizkid_-_Ojuelegba.mp3',
+        file: 'scr/music/_Wizkid_-_Ojuelegba.mp3'
+    },
+    {
+        title: 'Jon_Bellion_He Is The Same.mp3',
+        file: 'scr/music/01 He Is The Same.mp3'
+    },
+    {
+        title: 'Party-feat.-Gucci-Mane-Usher.mp3',
+        file: 'scr/music/01-Party-feat.-Gucci-Mane-Usher.mp3'
+    },
+    {
+        title: 'Khalid_Young Dumb & Broke - (Musicfire.in).mp3',
+        file: 'scr/music/02 - Young Dumb & Broke - (Musicfire.in).mp3'
+    } 
 ]
 let currentIndex = 0
-function next() {
-    currenAud.textContent = track[currentIndex] 
+function next()  {
+    const currentSong = track[currentIndex]
+    songTitle.textContent = currentSong.title
+    audio.scr = currentSong.file; 
     currentIndex = (currentIndex + 1) % track.length;
-    console.log(track[currentIndex])
-    console.log(currenAud.textContent)
+    console.log(audio)
+    audio.play()
 }
 nextbtn.addEventListener('click', () => {
     next()
-    audio.play()
+    
 })
