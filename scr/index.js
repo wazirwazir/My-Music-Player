27d9136be3034bdba9f408d2d595e40718b23167
wazirwazir <151955963+wazirwazir@users.noreply.github.com>
2024-06-21 17:19:23 +0100

    added progress bar

--- scr/index.js
+++ scr/index.js
@@ -5,20 +5,18 @@ let audio = document.getElementById('audio')
 let playsign = document.querySelector('.playsign')
 let pauseign = document.querySelector('.pausesign')
 let currenAud = document.querySelector('.currentAud')
-let songTitle = document.getElementById('songTitle')
+let songTitle = document.getElementById('marquee')
 let musicImg = document.querySelector('img')
+
 
 playbtn.addEventListener('click', () => {
    if(audio.paused) {
     audio.play()
+    playsign.classList.add('hide')
+    pauseign.classList.remove('hide')
    }
    else if (!audio.paused) {
     audio.pause()
-   }
-   if (audio.play) {
-    playsign.classList.add('hide')
-    pauseign.classList.remove('hide')
-   } else if (!audio.play) {
     playsign.classList.remove('hide')
     pauseign.classList.add('hide')
    }
@@ -32,8 +30,8 @@ let track = [
     },
     {
         image: 'scr/img/20230704_083855.jpg',
-        title: 'Wizkid_-_Ojuelegba.mp3',
-        file: 'scr/music/_Wizkid_-_Ojuelegba.mp3'
+        title: 'Unforgettable-Ft.-Swae-Lee',
+        file: 'scr/music/Unforgettable-Ft.-Swae-Lee-www.Moozika.ir_.mp3'
     },
     {
         image: 'scr/img/20230624_173024.jpg',
@@ -49,6 +47,8 @@ function next()  {
     audio.src = currentSong.file; 
     currentIndex = (currentIndex + 1) % track.length;
     audio.play()
+    playsign.classList.add('hide')
+    pauseign.classList.remove('hide')
 }
 function prev()  {
     const currentSong = track[currentIndex]
@@ -57,8 +57,22 @@ function prev()  {
     audio.src = currentSong.file; 
     currentIndex = (currentIndex - 1 + track.length) % track.length;
     audio.play()
+    playsign.classList.add('hide')
+    pauseign.classList.remove('hide')
 }
 
+function updateBar() {
+    const currentTime = audio.currentTime
+    const duration = audio.duration
+    const progressPercent = (currentTime / duration) * 100;
+    const dur = document.getElementsByClassName('dur')[0]
+    dur.style.width = progressPercent + '%'
+}
+
+
+
+audio.addEventListener('timeupdate', updateBar)
 audio.addEventListener('ended', next)
 nextbtn.addEventListener('click', next)
 prevbtn.addEventListener('click', prev)
+
