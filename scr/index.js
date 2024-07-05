79cf003b27f931db9c8b1f9f3ca45987f30b53c1
wazirwazir <151955963+wazirwazir@users.noreply.github.com>
2024-07-05 18:34:32 +0100

    corrected duration

--- scr/index.js
+++ scr/index.js
@@ -28,17 +28,20 @@ let track = [
     {
         image:'scr/img/20230703_155700.jpg',
         title: 'Freaky Friday (feat. Chris Brown)',
-        file: 'scr/music/01 Freaky Friday (feat. Chris Brown).mp3'
+        file: 'scr/music/01 Freaky Friday (feat. Chris Brown).mp3',
+        duration: '03:36'
     },
     {
         image: 'scr/img/20230704_083855.jpg',
         title: 'Unforgettable-Ft.-Swae-Lee',
-        file: 'scr/music/Unforgettable-Ft.-Swae-Lee-www.Moozika.ir_.mp3'
+        file: 'scr/music/Unforgettable-Ft.-Swae-Lee-www.Moozika.ir_.mp3',
+        duration: '03:55'
     },
     {
         image: 'scr/img/20230624_173024.jpg',
         title: 'Khalid_Young Dumb & Broke - (Musicfire.in).mp3',
-        file: 'scr/music/02 - Young Dumb & Broke - (Musicfire.in).mp3'
+        file: 'scr/music/02 - Young Dumb & Broke - (Musicfire.in).mp3',
+        duration: '03:23'
     } 
 ]
 let currentIndex = 0
@@ -46,6 +49,7 @@ function next()  {
     const currentSong = track[currentIndex]
     musicImg.src = currentSong.image;
     songTitle.textContent = currentSong.title;
+    totalDuration.textContent = currentSong.duration;
     audio.src = currentSong.file; 
     currentIndex = (currentIndex + 1) % track.length;
     audio.play()
@@ -55,6 +59,7 @@ function next()  {
 function prev()  {
     const currentSong = track[currentIndex]
     musicImg.src = currentSong.image;
+    totalDuration.textContent = currentSong.duration;
     songTitle.textContent = currentSong.title
     audio.src = currentSong.file; 
     currentIndex = (currentIndex - 1 + track.length) % track.length;
